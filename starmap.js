d5af68256bd6978492dff8452b14d8f397b24877
TheDigitalScorcerer <vermem2003@gmail.com>
2020-02-07 23:31:00 +0800

    Adapted the star map to be a function that returns an array

--- starmap.js
+++ starmap.js
@@ -1,7 +1,9 @@
-var starMap = [];
-
-for (let i = 0; i < 32; i++) {
-    for (let j = 0; j < 32; j++) {
-        starMap.push([i * 64, j * 64, 8]);
+function starMap() {
+    var map = [];
+    for (let i = 0; i < 32; i++) {
+        for (let j = 0; j < 32; j++) {
+            map.push([i * 64, j * 64, 8]);
+        }
     }
+    return map;
 }
