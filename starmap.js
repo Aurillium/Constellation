15a1cca8043d7b855a52324958cb924624d61fcf
MD5M-128 <31848971+TheDigitalScorcerer@users.noreply.github.com>
2020-02-11 17:35:27 +0800

    Merged all files into one

--- starmap.js
+++
@@ -1,9 +0,0 @@
-function starMap() {
-    var map = [];
-    for (let i = 0; i < 32; i++) {
-        for (let j = 0; j < 32; j++) {
-            map.push([i * 64, j * 64, 8]);
-        }
-    }
-    return map;
-}
